94d3be9252377c1f35f191be402d3464805ddce2
uncroos <any492266@gmail.com>
2024-12-05 09:18:05 +0900

    Update get_data.py

--- code/get_data.py
+++ code/get_data.py
@@ -1,8 +1,9 @@
+#padnas 불러오기
 import pandas as pd
 
-# Load the uploaded CSV file to inspect its contents
-file_path = '/mnt/data/diabetes.csv'
+#나의 데이터셋 불어로기
+file_path = '/Users/an-yohan/Documents/GitHub/BigData_Class/data/diabetes.csv'
 data = pd.read_csv(file_path)
 
-# Display the first few rows of the data to understand its structure
+#데이터의 앞부분만 출력하기
 data.head()
