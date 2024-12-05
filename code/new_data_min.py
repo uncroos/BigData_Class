8f59f0896e6850180b2656b1bfe7c4d722d743ef
uncroos <any492266@gmail.com>
2024-12-05 10:29:04 +0900

    Update new_data_min.py

--- code/new_data_min.py
+++ code/new_data_min.py
@@ -7,14 +7,10 @@ from scipy.stats import chi2_contingency
 data = pd.read_csv('/Users/an-yohan/Documents/GitHub/BigData_Class/data/new_data.csv')
 print(data.describe())
 
-# 1. BMI 분포에 따른 당뇨병 발생률
 data['BMI_Group'] = pd.cut(data['BMI'], bins=[0, 18.5, 25, 30, 100], 
                            labels=['Underweight', 'Normal', 'Overweight', 'Obese'])
 grouped = data.groupby('BMI_Group')['Outcome'].mean()
 print(grouped)
-
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 # 1. BMI 그룹별 당뇨병 발생률 (Barplot)
 grouped = data.groupby('BMI_Group')['Outcome'].mean()
@@ -30,10 +26,10 @@ sns.scatterplot(x='BMI', y='Glucose', hue='Outcome', data=data)
 plt.title("BMI vs Glucose by Diabetes Outcome")
 plt.show()
 
-# 3. BMI와 당뇨병 여부에 따른 BMI 분포 (Boxplot)
+# 3. BMI와 당뇨병 여부에 따른 체중 분포 (Boxplot)
 plt.figure(figsize=(8, 6))
 sns.boxplot(x='Outcome', y='BMI', data=data)
-plt.title("BMI Distribution by Diabetes Outcome")
+plt.title("Weight Distribution by Diabetes Outcome")
 plt.show()
 
 # 4. BMI와 혈당의 분포 (Histogram)
@@ -41,6 +37,7 @@ plt.figure(figsize=(8, 5))
 sns.histplot(data=data, x='BMI', kde=True, color='blue', bins=30)
 plt.title("Distribution of BMI")
 plt.show()
+
 plt.figure(figsize=(8, 5))
 sns.histplot(data=data, x='Glucose', kde=True, color='green', bins=30)
 plt.title("Distribution of Glucose Levels")
